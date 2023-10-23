f22567eff57f8a126433317ee6eacdc5d98a9c10
totsuka <techstart35@gmail.com>
2023-10-24 01:54:31 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       </div>
 
       {/* コンテンツ */}
-      <div className="mx-auto max-w-7xl px-3 sm:px-6 lg:px-8 bg-blue-50 pt-10">
+      <div className="mx-auto px-3 sm:px-6 lg:px-8 bg-blue-50 pt-5">
         <div className="mx-auto max-w-3xl pb-10 px-4 sm:px-6 lg:px-8">
           {/* About */}
           <div className="w-full mb-20">
--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -1,5 +1,6 @@
 const tabs = [
-  { name: "About", href: '#', current: true },
+  { name: "概要", href: '#', current: true },
+  { name: "画像", href: '#', current: false },
 ]
 
 function classNames(...classes: any) {
@@ -11,7 +12,7 @@ export default function Tabs() {
     <div>
       <div className="block">
         <div className="border-b border-gray-200">
-          <nav className="-mb-px flex space-x-8" aria-label="Tabs">
+          <nav className="-mb-px flex space-x-4" aria-label="Tabs">
             {tabs.map((tab) => (
               <a
                 key={tab.name}
@@ -20,7 +21,7 @@ export default function Tabs() {
                   tab.current
                     ? 'border-indigo-500 text-indigo-600'
                     : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                  'whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium'
+                  'whitespace-nowrap border-b-2 py-4 px-1 text-base font-medium'
                 )}
                 aria-current={tab.current ? 'page' : undefined}
               >
